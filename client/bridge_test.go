4c7e2cd94f7b7c021ce800d5eafbe65a22912710
Alex Ninneman <alex@ninneman.org>
2018-08-16 01:31:18 -0500

    sets opencensus trace sampler for tests

--- client/bridge_test.go
+++ client/bridge_test.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 
 	hue "github.com/ninnemana/huego"
-	"github.com/ninnemana/huego/client"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
 )
 
 var (
@@ -14,9 +17,27 @@ var (
 )
 
 func TestMain(m *testing.M) {
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: "ninneman-org",
+		OnError: func(err error) {
+			log.Fatal(err)
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer exporter.Flush()
 
-	var err error
-	cl, err = client.New()
+	trace.RegisterExporter(exporter)
+	defer trace.UnregisterExporter(exporter)
+	view.RegisterExporter(exporter)
+	defer view.UnregisterExporter(exporter)
+
+	trace.ApplyConfig(trace.Config{
+		DefaultSampler: trace.AlwaysSample(),
+	})
+
+	cl, err = New()
 	if err != nil {
 		log.Fatalf("failed to create new hue.Client: %v", err)
 	}
@@ -25,21 +46,27 @@ func TestMain(m *testing.M) {
 }
 
 func TestAllBridges(t *testing.T) {
-	ctx := context.Background()
 
-	results, err := cl.AllBridges(ctx, &hue.AllBridgeParams{})
+	_, span := trace.StartSpan(context.Background(), "test.hue.http")
+	defer span.End()
+
+	ctx := trace.NewContext(context.Background(), span)
+
+	results, err := cl.AllBridges(ctx, &hue.AllBridgeParams{
+		Method: "remote",
+	})
 	if err != nil {
 		t.Errorf("failed to get all briges: %v", err)
 		return
 	}
 
 	for _, res := range results {
-		bridge, ok := res.(*hue.Bridge)
+		bridge, ok := res.(map[string]interface{})
 		if !ok {
-			t.Errorf("expected '*hue.Bridge' got '%T'", res)
+			t.Errorf("expected 'map[string]interface{}' got '%T'", res)
 			continue
 		}
 
-		t.Log(bridge.GetName())
+		t.Log(bridge["internalipaddress"])
 	}
 }
